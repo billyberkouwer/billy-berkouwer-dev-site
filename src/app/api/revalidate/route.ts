e9f0025ce840b312511ff4af46fcb0a230b87a79
billy.berkouwer <billy@ocove.studio>
2023-11-30 09:08:14 +0000

    remove edge and dynamic from api route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidatePath, revalidateTag } from 'next/cache'
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
 export async function POST(request: NextRequest) {
     const route = request.nextUrl.searchParams.get('route');
     const tag = request.nextUrl.searchParams.get('tag');
