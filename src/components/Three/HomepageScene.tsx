054770f7ab21a6db2a401ab1ff0c91af0923ae7e
billy.berkouwer <billy@ocove.studio>
2023-11-30 18:54:58 +0000

    dynamic path for gltf

--- src/components/Three/HomepageScene.tsx
+++ src/components/Three/HomepageScene.tsx
@@ -12,7 +12,7 @@ export default function HomepageScene() {
     const lightRef = useRef<Object3D<Object3DEventMap>>() as MutableRefObject<Object3D<Object3DEventMap>>;
     const noise = createNoise4D();
     const { scene, clock } = useThree();
-    const gltf = useGLTF('/Plant-2-bake-bones.gltf');
+    const gltf = useGLTF(process.env.NODE_ENV === 'production' ? 'https://billy-berkouwer-dev.vercel.app/Plant-2-bake-bones.gltf' : '/Plant-2-bake-bones.gltf');
     const bonesRef = useRef<Bone[]>();
     const bonesRestPos = useRef<{ x: number, y: number, z: number }[]>();
     const bonesRestRot = useRef<{ x: number, y: number, z: number }[]>();
