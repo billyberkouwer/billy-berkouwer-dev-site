a96dcbaf55bd2f48d9c8baf20e11ba1ee7f350a5
billy.berkouwer <billy@ocove.studio>
2024-03-05 11:44:07 +0000

    fix merge syntax error

--- src/app/(website)/work/page.tsx
+++ src/app/(website)/work/page.tsx
@@ -2,11 +2,6 @@ import WorkLayout from "@/components/Work/WorkLayout";
 import { logoLocations } from "@/lib/locations/logos";
 
 export default function WorkPage() {
-<<<<<<< HEAD
-    // redirect('/')
-    return <WorkLayout />
-=======
 
     return <WorkLayout logos={logoLocations} />
->>>>>>> main
 }
