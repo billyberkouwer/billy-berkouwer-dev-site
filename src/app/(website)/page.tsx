4824fb0dce617182a089ff72ea3801685fdab804
billy.berkouwer <billy@ocove.studio>
2024-03-08 11:24:07 +0000

    update metadescription

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,13 +1,15 @@
 import HomeLayout from "@/components/Home/HomeLayout";
 
-import type { Metadata } from 'next'
- 
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'Billy Myles-Berkouwer | 3D & Web Development',
-  description: 'Billy is a full-stack web developer and 3D designer.',
-  keywords: 'Web Development, 3D Artist, 3D Designer, Web Design, Fine Art',
-}
+  title: "Billy Myles-Berkouwer | 3D & Web Development",
+  description: `Billy Myles-Berkouwer is a full-stack web developer, visual artist,
+  and technical lead on 3D web projects for clients in arts and
+  commercial sectors.`,
+  keywords: "Web Development, 3D Artist, 3D Designer, Web Design, Fine Art",
+};
 
 export default function Home() {
-  return <HomeLayout />
+  return <HomeLayout />;
 }
