3b60dea5fcc81c6f6d7dd0377f135c36b7daa030
billy.berkouwer <billy@ocove.studio>
2024-03-08 11:18:28 +0000

    tweak bio text

--- src/components/Home/InfoBox.tsx
+++ src/components/Home/InfoBox.tsx
@@ -13,8 +13,12 @@ export default function InfoBox() {
       <div className={`expander ${isInfoBoxOpen ? "expanded" : ""}`}>
         <div className={`expander-content ${isInfoBoxOpen ? "expanded" : ""}`}>
           <p>
-            Billy Myles-Berkouwer is a full-stack web developer, visual artist, and technical lead for 3D web projects for clients in commercial and arts sectors. 
-            With strong interpersonal and technical skills, he is comfortable working across various project phases, including art direction, 3D design, and front-end and back-end web development.
+            Billy Myles-Berkouwer is a full-stack web developer, visual artist,
+            and technical lead on 3D web projects for clients in arts and
+            commercial sectors. With strong interpersonal and technical skills,
+            he is comfortable working across various project phases, including
+            art direction, 3D design and asset creation, 3D animation, and
+            front-end and back-end web development.
           </p>
           <p>
             About the flower - I modelled and procedurally textured this flower
@@ -26,9 +30,15 @@ export default function InfoBox() {
             <a href="mailto:billy@ocove.studio"> contact me here</a>{" "}
           </p>
           <ul className="list__social-links">
-            <li><a href="https://www.linkedin.com/in/billyberkouwer">LinkedIn</a></li>
-            <li><a href="https://www.instagram.com/billyberkouwer/">Instagram</a></li>
-            <li><a href="https://github.com/billyberkouwer">GitHub</a></li>
+            <li>
+              <a href="https://www.linkedin.com/in/billyberkouwer">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/billyberkouwer/">Instagram</a>
+            </li>
+            <li>
+              <a href="https://github.com/billyberkouwer">GitHub</a>
+            </li>
           </ul>
         </div>
       </div>
